66682ab6116beb71d6aacdea28f681ff42c375a1
kanishk-8 <kanishkkumar222004@gmail.com>
2025-06-07 19:21:41 +0530

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Kanishk - Web & App Developer",
+  description: "Student and Full Stack Developer specializing in React, Next.js, React Native, Golang, Flask, and more. Explore my projects and skills.",
+  keywords: "Web Developer, App Developer, React, Next.js, React Native, Golang, Flask, Tailwind CSS",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
